007dd250606f3444f3267b3d6e1f3de5f1e1882b
B1ll1 <rodolfo_zahn@yahoo.com.br>
2016-11-16 15:39:45 -0200

    migrations finais

---
+++ database/migrations/2016_11_16_173721_create_product_country_types_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductCountryTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_country_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_code')->unsigned()->index();
+            $table->foreign('product_code')
+                 ->references('code')->on('products');
+            $table->integer('country_code')->unsigned()->index();
+            $table->foreign('country_code')
+                 ->references('code')->on('countries');
+            $table->integer('type_code')->unsigned()->index();
+            $table->foreign('type_code')
+                 ->references('code')->on('types');
+            $table->integer('year');
+            $table->integer('planted_area');
+            $table->integer('harvested_area');
+            $table->integer('production');
+            $table->decimal('value',15,2);
+            $table->integer('yield');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_country_types');
+    }
+}
